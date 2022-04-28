a48e364f1d1cd0610f69ea24b0830ed324784239
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-29 03:02:58 +0600

    Database connected and all the inventories from database are loaded

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT||5000;
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 const app = express();
 //Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Auto Xpress Server Connected!')
-})
-app.listen(port,()=>{
-    console.log('Server Connected, Enjoy!');
-})
+app.get("/", (req, res) => {
+  res.send("Auto Xpress Server Connected!");
+});
+app.listen(port, () => {
+  console.log("Server Connected, Enjoy!");
+});
+//Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lywwz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client
+      .db("auto-xpress")
+      .collection("inventory");
+    //Displaying all inventory collection
+    app.get("/inventories", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
